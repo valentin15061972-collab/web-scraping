1e384912fdf1ca9bb6cb91f8692524770e3ca051
Valentin <valentin15061972@gmail.com>
2026-03-04 19:24:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,25 +6,30 @@ from fake_headers import Headers
 KEYWORDS = ['дизайн', 'фото', 'web', 'python']
 
 url = 'https://habr.com/ru/articles/'
-headers = Headers(browser='chrome', os='windows').generate()
-response = requests.get(url, headers=headers)
 
-soup = bs4.BeautifulSoup(response.text, features='lxml')
-articles_block = soup.select_one('div.tm-articles-list')
-articles_list = articles_block.select('article.tm-articles-list__item')
 
-for article in articles_list:
-    for el in KEYWORDS:
-        if el in article.text.lower():
-            article_with_link = article.select_one('h2.tm-title.tm-title_h2')
-            link = 'https://habr.com' + article_with_link.select_one('a')['href']
+def parsing_articles():
+    headers = Headers(browser='chrome', os='windows').generate()
+    response = requests.get(url, headers=headers)
+    soup = bs4.BeautifulSoup(response.text, features='lxml')
+    articles_block = soup.select_one('div.tm-articles-list')
+    articles_list = articles_block.select('article.tm-articles-list__item')
 
-            response = requests.get(link)
-            article = bs4.BeautifulSoup(response.text, features='lxml')
-            data = article.select_one('time')['title']
-            header = article.select_one('h1').text.strip()
+    for article in articles_list:
+        for el in KEYWORDS:
+            if el in article.text.lower():
+                article_with_link = article.select_one('h2.tm-title.tm-title_h2')
+                link = 'https://habr.com' + article_with_link.select_one('a')['href']
 
-            print(f'<{data}> - <{header}> - <{link}>')
-            break
+                response = requests.get(link)
+                article = bs4.BeautifulSoup(response.text, features='lxml')
+                data = article.select_one('time')['title']
+                header = article.select_one('h1').text.strip()
+
+                print(f'<{data}> - <{header}> - <{link}>')
+                break
+
+
+parsing_articles()
 
 
